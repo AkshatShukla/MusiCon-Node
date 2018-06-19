3a64a78147951dfc5e90737fbbcb7120b6995c02
patelvp <patel.visha@husky.neu.edu>
2018-06-19 16:21:28 -0400

    First commit for node.js server

---
+++ server.js
@@ -0,0 +1,71 @@
+var express = require('express')
+var bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://heroku_m9j3xqd6:cqo386da2m9s1lhqui3roqbn2b@ds263710.mlab.com:63710/heroku_m9j3xqd6');//db url
+
+
+var app = express()
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin",
+    "https://assignment4-angular.herokuapp.com");
+  res.header("Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
+
+
+
+
+var session = require('express-session')
+app.use(session({
+  resave: false,
+  saveUninitialized: true,
+  secret: 'any string'
+}));
+
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.get('/', function (req, res) {
+  var theMessage = req.params['theMessage'];
+  res.send(theMessage);
+})
+
+app.get('/api/session/set/:name/:value',
+  setSession);
+app.get('/api/session/get/:name',
+  getSession);
+// app.get('/api/session/get',
+//   getSessionAll);
+// app.get('/api/session/reset',
+//   resetSession);
+
+function setSession(req, res) {
+  var name = req.params['name'];
+  var value = req.params['value'];
+  req.session[name] = value;
+  res.send(req.session);
+}
+
+function getSession(req, res) {
+  var name = req.params['name'];
+  var value = req.session[name];
+  res.send(value);
+}
+
+
+var userService = require('./services/user.service.server');
+userService(app);
+
+var sectionService = require('./services/section.service.server');
+sectionService(app);
+app.listen(process.env.PORT || 4000);
